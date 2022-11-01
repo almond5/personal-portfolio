ee31e73c3a4c98ba6cae8a78d4c01acc5a080652
Adrian Hossen <adrianhossen5@gmail.com>
2022-10-31 22:34:37 -0400

    a

--- pages/Navbar.tsx
+++ pages/Navbar.tsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-30 z-[100]'>
-      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-        <Image
-          src='/../public/logo.jpg'
-          alt='/'
-          width='100'
-          height='100'
-        />
+    <div>
+      <div className='fixed w-full h-20 shadow-xl z-[100]'>
+        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+          <Image
+            src='/../public/logo.png'
+            alt='/'
+            width='200'
+            height='160'
+          />
+        </div>
+      </div>
+      <div className='ml-2'>
+        <ul className = 'items-center hover:border-b'>
+          <Link href='/'>
+            <li className=''>Home</li>
+          </Link>
+        </ul>
+        <div className = 'md:hidden'>
+          <AiOutlineMenu size={25} />
+        </div>
       </div>
     </div>
   )
 }
 
 export default Navbar
+
+
