f76e12bb4416a0b339da0861b9159c1022178e8a
Adrian Hossen <adrianhossen5@gmail.com>
2022-11-13 18:58:40 -0500

    a

--- pages/Hero.tsx
+++ pages/Hero.tsx
@@ -5,10 +5,10 @@ const Hero = () => {
   return (
     <div id='home' className='py-80 flex-col justify-center text-center'>
       <div className='mb-3 text-2xl leading-2;'>Hello, my name is</div>
-      <div className='mb-5 text-3xl font-semibold md:text-5xl lg:text-6xl xl:text-6xl'>
+      <div className='mb-5 text-5xl font-semibold xs:text-5xl sm:text-6xl md:text-6xl lg:text-6xl xl:text-6xl'>
         Adrian Hossen.
       </div>
-      <div className='mx-auto max-w-md text-xl text-left'>
+      <div className='mx-auto max-w-md text-md text-left lg:text-xl'>
         I am a software developer with a growth mindset who is always eager to
         learn more. I love programming and sharing my knowledge with others!
         Please contact me with any questions or if you just wish to chat.
