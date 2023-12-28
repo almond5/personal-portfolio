ae7a0237af9921503d147d250507ce3d8702dab3
Adrian Hossen <adrianhossen5@gmail.com>
2023-12-28 14:58:26 -0500

    WIP

---
+++ src/components/FooterHome.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+import React from 'react';
+import { FaEnvelope, FaGithub, FaLinkedin, FaNewspaper } from 'react-icons/fa';
+
+const FooterHome = () => {
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        bottom: 0,
+        width: '100%',
+        textAlign: 'center',
+        padding: '10px 0', // Optional: Add padding for better styling
+      }}
+    >
+      <div className="mb-3">
+        <button>
+          <Link href="https://www.linkedin.com/in/adrian5/">
+            <FaLinkedin className="text-3xl" />
+          </Link>
+        </button>
+        <button className='px-4'>
+          <Link href="https://github.com/almond5">
+            <FaGithub className="text-3xl" />
+          </Link>
+        </button>
+        <button>
+          <Link href="mailto:adrianhossen4@gmail.com">
+            <FaEnvelope className="text-3xl" />
+          </Link>
+        </button>
+        <button className='px-4'>
+          <Link href='resume.pdf'>
+            <FaNewspaper className="text-3xl" />
+          </Link>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FooterHome;
