a8382e2a078d54ca81460a0827dd83fd1cac566d
Adrian Hossen <adrianhossen5@gmail.com>
2023-12-31 15:43:41 -0500

    WIP

--- pages/Navbar.tsx
+++ pages/Navbar.tsx
@@ -1,9 +1,12 @@
 import router from 'next/router';
 import React, { useState, useEffect, useCallback } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import MobileSideBar from '../src/components/mobileSideBar';
 
 const Navbar = () => {
   const [showNav, setShowNav] = useState(false);
   const [scrollY, setScrollY] = useState(0);
+  const [mobileSidebar, setMobileSideBar] = useState(false);
 
   const handleScroll = useCallback(() => {
     window.scrollY - scrollY > 0 ? setShowNav(false) : setShowNav(true);
@@ -19,11 +22,14 @@ const Navbar = () => {
 
   return (
     <div className="max-w-screen-lg">
+      <div className={`${mobileSidebar ? 'md:hidden' : 'hidden md:hidden'}`}>
+        <MobileSideBar></MobileSideBar>
+      </div>
       <div
-        className={`fixed z-10 flex w-full items-center overflow-hidden px-8 transition-all ease-in-out 
+        className={`fixed z-9 flex w-full items-center overflow-hidden px-8 transition-all ease-in-out 
         ${
           showNav && scrollY != 0
-            ? 'h-28 shadow-md backdrop-blur-md'
+            ? 'h-24 shadow-md backdrop-blur-md'
             : `${
                 scrollY == 0
                   ? 'h-28 shadow-none backdrop-blur-md'
@@ -33,6 +39,11 @@ const Navbar = () => {
       >
         <div className="grid place-items-end w-full h-8">
           <div>
+            <div className="flex items-center md:hidden">
+              <button onClick={() => setMobileSideBar(!mobileSidebar)}>
+                <MenuIcon fontSize="large" />
+              </button>
+            </div>
             <ul className="hidden items-center md:flex">
               <li className="px-4 font-bold text-2xl">
                 <button onClick={() => router.push('/Home')}>Home</button>
