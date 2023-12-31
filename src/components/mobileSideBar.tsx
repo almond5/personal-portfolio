92d24f5fe01e5b73212a1ce6433c6d1a1509e9ee
Adrian Hossen <adrianhossen5@gmail.com>
2023-12-31 15:43:49 -0500

    WIP

---
+++ src/components/mobileSideBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MobileSidebar = () => {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full bg-gray-800 text-white p-4">
+      {/* Add your mobile sidebar content here */}
+      <div></div>
+    </div>
+  );
+};
+
+export default MobileSidebar;
