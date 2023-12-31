ab2fd0fff793634974dda51df2fd7628c8087a21
Adrian Hossen <adrianhossen5@gmail.com>
2023-12-31 14:42:04 -0500

    WIP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import Navbar from '../src/components/Navbar';
+import Navbar from './Navbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
